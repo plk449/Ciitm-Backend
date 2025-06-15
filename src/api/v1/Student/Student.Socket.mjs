9609d232a7478962ae080da7e561687cfed4511d
Abhishek Gupta <abhishek9661342993@gmail.com>
2025-06-15 15:56:02 +0000

    refactor: Enhance error handling and improve student not found message in Student socket

--- src/api/v1/Student/Student.Socket.mjs
+++ src/api/v1/Student/Student.Socket.mjs
@@ -3,27 +3,28 @@ import { response } from "express";
 import Admission from "../Admission/Admission.model.mjs";
 import AdmissionUtils from "../Admission/Admission.utils.mjs";
 import StudentUtils from "./Student.utils.mjs";
+import StudentConstant from "./Student.constant.mjs";
 
 
 const SocketToFindStudentById = (io, socket) => {
   socket.on('findStudentById', async ({studentId}) => {
     try {
-      console.log('Socket to find student by ID:', studentId);
+    
       const student = await StudentUtils.FindByStudentId(studentId);
-      console.log('Found student:', student);
+    
       if (!student) {
-        throw new Error('Student not found');
+        throw new Error(StudentConstant.STUDENT_NOT_FOUND);
       }
       socket.emit('studentFound', student);
     } catch (error) {
       console.error('Error finding student:', error);
-      socket.emit('error', { message: 'Error finding student', error: error.message });
+      socket.emit('error', { message: error.message });
     }
   });
 }
 
 const StudentSocket = (io, socket) => {
-    console.log('Student Socket connected:', socket.id);
+
    SocketToFindStudentById(io, socket);
 }
 export default StudentSocket;
